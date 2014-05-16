55c58ca6e1b0285da889ce9419f1e12aef96a755
mdrustad <mdrustad@PC3773.ad.northcentral.edu>
2014-05-16 16:33:21 -0500

    Fleshed out FullCardSet methods. Sped up Card creation a tad

--- src/swccgManager/Card.java
+++ src/swccgManager/Card.java
@@ -17,15 +17,15 @@ import java.sql.*;
  */
 public class Card {
 	
-	private int cardId;
-	private String cardName;
-	private String side;
-	private String cardType;
-	private String subType;
-	private String expansion;
-	private String rarity;
-	private String uniqueness;
-	private Image frontSideImage;
+	private int m_cardId;
+	private String m_cardName;
+	private String m_side;
+	private String m_cardType;
+	private String m_subType;
+	private String m_expansion;
+	private String m_rarity;
+	private String m_uniqueness;
+	private Image m_frontSideImage;
 	
 	/**
 	 * Creates a card by pulling from the database all the important information
@@ -36,93 +36,116 @@ public class Card {
 	{
 		//Set all the information
 		ResultSet cardInfo = GenericSQLQueries.getCardVitals(swdb, cardId);
-		this.cardId = cardId;
+		this.m_cardId = cardId;
 		try {
-			cardName = cardInfo.getString("cardName");
-			side = cardInfo.getString("Grouping");
-			cardType = cardInfo.getString("CardType");
-			subType = cardInfo.getString("SubType");
-			expansion = cardInfo.getString("Expansion");
-			rarity = cardInfo.getString("Rarity");
-			uniqueness = cardInfo.getString("Uniqueness");
+			m_cardName = cardInfo.getString("cardName");
+			m_side = cardInfo.getString("Grouping");
+			m_cardType = cardInfo.getString("CardType");
+			m_subType = cardInfo.getString("SubType");
+			m_expansion = cardInfo.getString("Expansion");
+			m_rarity = cardInfo.getString("Rarity");
+			m_uniqueness = cardInfo.getString("Uniqueness");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
 		ImageManager im = new ImageManager(swdb);
-		frontSideImage = im.getCardImage(cardId, 1);//1 is always the front side
+		m_frontSideImage = im.getCardImage(cardId, 1);//1 is always the front side
 	}
-	
+	/**
+	 * Creates a card, with full paramaters (this should aid in speed of retrieval, instead of calling a new query every time)
+	 * @param cardId
+	 * @param cardName
+	 * @param side
+	 * @param cardType
+	 * @param subType
+	 * @param expansion
+	 * @param rarity
+	 * @param uniqueness
+	 * @param img Front side image of the card
+	 */
+	public Card (int cardId, String cardName, String side, String cardType, String subType, String expansion, String rarity, String uniqueness, Image img)
+	{
+		m_cardId = cardId;
+		m_cardName = cardName;
+		m_side = side;
+		m_cardType = cardType;
+		m_subType = subType;
+		m_expansion = expansion;
+		m_rarity = rarity;
+		m_uniqueness = uniqueness;
+		m_frontSideImage = img;
+	}
 	/**
 	 * Returns the full name as it would be printed on the card
 	 */
 	@Override
 	public String toString()
 	{
-		String fullName = uniqueness + cardName;
+		String fullName = m_uniqueness + m_cardName;
 		return fullName;
 	}
 	/**
 	 * @return the cardId
 	 */
 	public int getCardId() {
-		return cardId;
+		return m_cardId;
 	}
 
 	/**
 	 * @return the cardName
 	 */
 	public String getCardName() {
-		return cardName;
+		return m_cardName;
 	}
 
 	/**
 	 * @return the side
 	 */
 	public String getSide() {
-		return side;
+		return m_side;
 	}
 
 	/**
 	 * @return the cardType
 	 */
 	public String getCardType() {
-		return cardType;
+		return m_cardType;
 	}
 
 	/**
 	 * @return the subType
 	 */
 	public String getSubType() {
-		return subType;
+		return m_subType;
 	}
 
 	/**
 	 * @return the expansion
 	 */
 	public String getExpansion() {
-		return expansion;
+		return m_expansion;
 	}
 
 	/**
 	 * @return the rarity
 	 */
 	public String getRarity() {
-		return rarity;
+		return m_rarity;
 	}
 
 	/**
 	 * @return the uniqueness
 	 */
 	public String getUniqueness() {
-		return uniqueness;
+		return m_uniqueness;
 	}
 
 	/**
 	 * @return the frontSideImage
 	 */
 	public Image getFrontSideImage() {
-		return frontSideImage;
+		return m_frontSideImage;
 	}
 
 	
--- src/swccgManager/FullCardSet.java
+++ src/swccgManager/FullCardSet.java
@@ -3,6 +3,7 @@
  */
 package swccgManager;
 
+import java.awt.*;
 import java.sql.*;
 import java.util.*;
 
@@ -30,19 +31,81 @@ public class FullCardSet {
 		try {
 			while(fullCardList.next())
 			{
+				//Retrieve card info from the row
 				int cardID = fullCardList.getInt("id");
+				String cardName = fullCardList.getString("cardName");
+				String side = fullCardList.getString("Grouping");
+				String cardType = fullCardList.getString("CardType");
+				String subType = fullCardList.getString("SubType");
+				String expansion = fullCardList.getString("Expansion");
+				String rarity = fullCardList.getString("Rarity");
+				String uniqueness = fullCardList.getString("Uniqueness");
 				
-				Card newCard = new Card (swdb, cardID);
+				ImageManager im = new ImageManager(swdb);
+				Image img = im.getCardImage(cardID, 1);//1 is always the front side
+				
+				Card newCard = new Card (cardID, cardName, side, cardType, subType, expansion, rarity, uniqueness, img);
 				cardSet.put(cardID, newCard);
 				numSuccessfullyAddedCards++;
-				//System.out.println("Created " + numSuccessfullyAddedCards + " cards, Card ID: " + cardID);
+				System.out.println("Created " + numSuccessfullyAddedCards + " cards, Card ID: " + cardID);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Created " + numSuccessfullyAddedCards + " cards.");
+		System.out.println("Created " + numSuccessfullyAddedCards + " cards out of exepected " + numCards + " number of cards");
 		
+	}
+	
+	/**
+	 * Returns a single card from the card set
+	 * @param cardId unique identifier for the cards
+	 * @return One starwars card owning the valid index
+	 */
+	public Card retrieveCard(int cardId)
+	{
+		Card card = cardSet.get(cardId);
+		return card;
+	}
+	/**
+	 * Takes an array of Card ID's and returns the list of cards
+	 * @param cardIdList List of the card ID's
+	 * @return Returns the matching set of cards
+	 */
+	public Card[] retrieveCardSet(int[] cardIdList)
+	{
+		//Create card array
+		int numCards = cardIdList.length;
+		Card[] cardList = new Card[numCards];
+		
+		//fill card array
+		for(int cardIndex : cardIdList)
+		{
+			Card newCard = retrieveCard(cardIndex);
+			cardList[cardIndex] = newCard;
+		}
+		
+		//return it
+		return cardList;
+	}
+	
+	/**
+	 * Returns the entire Card set as an array
+	 * @return Card list representation of the full card list hashtable
+	 */
+	public Card[] getFullCardSet()
+	{
+		Card[] fullCardList;
+		fullCardList = (Card[])cardSet.values().toArray();
+		
+		return fullCardList;
+	}
+	
+	/**
+	 * @return the numCards
+	 */
+	public int getNumCards() {
+		return cardSet.size();
 	}
 
 }
